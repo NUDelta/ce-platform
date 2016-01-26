224955fa1a31b71b26888535ac3eeb2bdbb4a829
Ryan Madden <ryanmadden2017@u.northwestern.edu>
2016-01-25 19:42:52 -0600

    Send emails when the experience ends

--- client/views/admin/experience_button.js
+++ client/views/admin/experience_button.js
@@ -11,5 +11,15 @@ Template.experienceButton.events({
             'Event is starting!',
             this.startEmailText, this._id);
       alert(`Sent ${this.name}`);
+  },
+    'click .end-btn': function(e) {
+      e.preventDefault();
+      let endEmailText = '<p>The experience has ended. Thanks for participating! Click <a href="' + this.endEmailLink + this._id + '">this link</a> to see the results.</p>';
+      Meteor.call('sendEmail',
+            'shannonnachreiner2012@u.northwestern.edu',
+            'Your experience has ended.',
+            endEmailText,
+            this._id);
+      alert(`Sent ${this.name}`);
   }
 });
