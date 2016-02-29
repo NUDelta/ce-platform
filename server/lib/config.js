b3547ed1f874ca3dd9dd824d603dd81ce7426454
aspin <tsunami70875@gmail.com>
2016-02-28 20:20:08 -0600

    missed a few lines

--- server/lib/config.js
+++ server/lib/config.js
@@ -2,7 +2,6 @@ Cerebro.NOTIFY_ALL = true;
 
 BrowserPolicy.content.allowSameOriginForAll();
 BrowserPolicy.content.allowOriginForAll('http://meteor.local');
-BrowserPolicy.content.allowOriginForAll('https://yourapp.io');
-BrowserPolicy.content.allowOriginForAll('https://*.yourapp.io');
-BrowserPolicy.content.allowOriginForAll('https://*.stripe.com');
+BrowserPolicy.content.allowOriginForAll('http://yo-star.xyz');
+BrowserPolicy.content.allowOriginForAll('http://aspin.xyz');
 BrowserPolicy.content.allowEval();
