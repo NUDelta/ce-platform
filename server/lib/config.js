9e2e76b3594e044b4f629c7da7b2c85c452701d0
aspin <tsunami70875@gmail.com>
2016-03-05 17:31:32 -0600

    commenting out push debug

--- server/lib/config.js
+++ server/lib/config.js
@@ -1,5 +1,7 @@
 Cerebro.NOTIFY_ALL = true;
 Cerebro.NOTIFY_METHOD = Cerebro.PUSH;
+//Cerebro.DEBUG_PUSH = true;
+//Cerebro.DEBUG_USERS = [ 'pTeAq958AvmvMvF7e', 'mr9qe4nRHQn8KufLX', 'BvYfcgvJ7yDETLjME' ];
 
 BrowserPolicy.content.allowSameOriginForAll();
 BrowserPolicy.content.allowOriginForAll('http://meteor.local');
