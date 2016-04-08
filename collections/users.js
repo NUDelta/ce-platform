e7df477960f51ca09e72fd6c68ad258d7b9209c1
Ryan Madden <ryanmadden2017@u.northwestern.edu>
2016-04-08 18:44:23 -0500

    Add pastIncidents to users schema

--- collections/users.js
+++ collections/users.js
@@ -28,6 +28,11 @@ Schema.Profile = new SimpleSchema({
     type: [String],
     label: 'Active experiences',
     optional: true
+  },
+  pastIncidents: {
+    type: [String],
+    label: 'Previous incidents the user was notified of',
+    optional: true
   }
 });
 
