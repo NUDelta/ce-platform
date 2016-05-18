0c49dcc4a6ca38e0b76cc0eea3c67397cc7e1480
Ryan Madden <ryanmadden2017@u.northwestern.edu>
2016-05-18 18:33:43 -0500

    Add signed-out messages and consistent header formatting

--- imports/ui/pages/profile.js
+++ imports/ui/pages/profile.js
@@ -3,3 +3,9 @@ import './profile.html';
 import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
 import { Router } from 'meteor/iron:router';
+
+Template.profile.helpers({
+  pastIncidents: function () {
+    return Meteor.user().profile.pastIncidents;
+  }
+});
