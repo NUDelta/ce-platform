af87ea73970d437221b86ad4ac7d7808f90ae4e8
Ryan Madden <ryanmadden2017@u.northwestern.edu>
2016-05-18 18:26:50 -0500

    Add list of experiences to browse page

--- imports/ui/pages/browse.js
+++ imports/ui/pages/browse.js
@@ -3,3 +3,15 @@ import './browse.html';
 import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
 import { Router } from 'meteor/iron:router';
+
+import { Experiences } from '../../api/experiences/experiences.js';
+
+Template.browse.onCreated(function() {
+  this.subscribe('experiences');
+});
+
+Template.browse.helpers({
+  experiences: function () {
+    return Experiences.find().fetch();
+  }
+});
