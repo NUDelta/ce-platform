4c76a87704b9e18b46bebb039cb3cb7a60143616
allisonsun <allisunson@gmail.com>
2017-02-26 13:17:58 -0600

    trying to fix camera crash

--- mobile-config.js
+++ mobile-config.js
@@ -3,3 +3,11 @@ App.accessRule('blob:*');
 App.accessRule('*.google.com/*');
 App.accessRule('*.googleapis.com/*');
 App.accessRule('*.gstatic.com/*');
+App.appendToConfig(`<platform name="ios">
+    <config-file platform="ios" target="*-Info.plist" parent="NSPhotoLibraryUsageDescription">
+      <string>YOUR DESCRIPTION (PHOTOS PERMISSION) HERE</string>
+    </config-file>
+    <config-file platform="ios" target="*-Info.plist" parent="NSCameraUsageDescription">
+      <string>YOUR DESCRIPTION (CAMERA PERMISSION) HERE</string>
+    </config-file>
+  </platform>`);
