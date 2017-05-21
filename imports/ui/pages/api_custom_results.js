4e135775bdbb25f375c597704ea6ac068b5816f0
jenniewerner <jenniewerner2018@u.northwestern.edu>
2017-05-21 16:08:15 -0500

    storybook results ui is upppp, legggoo

--- imports/ui/pages/api_custom_results.js
+++ imports/ui/pages/api_custom_results.js
@@ -1,5 +1,5 @@
 import './api_custom_results.html';
-
+import './storyPageResults.html'
 import '../components/displayImage.html';
 
 import { Meteor } from 'meteor/meteor';
@@ -17,6 +17,7 @@ import { TextEntries } from '../../api/text-entries/text-entries.js';
 import { Submissions } from '../../api/submissions/submissions.js';
 
 import { Incidents } from '../../api/incidents/incidents.js';
+
 
 Template.api_custom_results.helpers({
   data2pass(){
@@ -40,6 +41,9 @@ Template.registerHelper( 'getImage', (id) => {
 });
 Template.registerHelper( 'getText', (id) => {
   var text = TextEntries.findOne({_id: id});
+  console.log('text: ', text.text);
+  console.log('getting text for ', id)
+  
   return text.text;
 });
 
@@ -78,4 +82,69 @@ Template.api_custom_results.onCreated(function() {
       });
     }
   });
+});
+
+
+
+///storybook
+var slideIndex = 1;
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  
+  var slides = document.getElementsByClassName("mySlides");
+
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  
+  slides[slideIndex-1].style.display = "block";  
+}
+
+
+Template.storyPageResults.onCreated(function() {
+  this.autorun(() => {
+    console.log("loaded")
+    window.onload = function () { 
+      showSlides(slideIndex);
+    } 
+  });
+});
+Template.storyPageResults.helpers({
+  getNextSentenceId(photoIndex){
+    const instance = Template.instance()
+    
+    var submission = instance.data.submissions[photoIndex-1];
+    console.log(submission)
+    console.log(submission.content.nextSentence)
+    return submission.content.nextSentence
+
+  },
+  shouldLoadText(index){
+    return index > 0;
+  }
+});
+
+
+Template.storyPageResults.events({
+  'click .prev'(event, instance) {
+    event.preventDefault();
+    console.log("LEFTT");
+    plusSlides(-1)
+  },
+  'click .next'(event, instance) {
+    event.preventDefault();
+    console.log("RIGHT");
+    plusSlides(1)
+  }
 });
