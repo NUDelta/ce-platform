b35b4bb00289d2a6494619a9fc571cb8dd3860c2
jenniewerner <jenniewerner2018@u.northwestern.edu>
2018-01-31 14:18:22 -0600

    adding logging for location to find the problem with the location package

---
+++ imports/api/locations/location_log.js
@@ -0,0 +1,44 @@
+import { Mongo } from 'meteor/mongo';
+import { SimpleSchema } from 'meteor/aldeed:simple-schema';
+import { Schema } from '../schema.js';
+
+export const Location_log = new Mongo.Collection('Location_log');
+
+
+Schema.Location_log = new SimpleSchema({
+  uid: {
+    type: String,
+    regEx: SimpleSchema.RegEx.Id,
+    label: 'user id'
+  },
+  lat: {
+    type: Number,
+    decimal: true,
+    min: -90,
+    max: 90
+  },
+  lng: {
+    type: Number,
+    decimal: true,
+    min: -180,
+    max: 180
+  },
+  time: {
+    type: Number,
+    optional: true
+  }
+});
+
+Location_log.attachSchema(Schema.Location_log);
+
+Location_log.allow({
+  insert: function() {
+    return true;
+  },
+  update: function() {
+    return true;
+  },
+  remove: function() {
+    return true;
+  }
+});
--- imports/startup/server/router.js
+++ imports/startup/server/router.js
@@ -2,6 +2,7 @@ import { Router } from 'meteor/iron:router';
 
 import { updateUserLocationAndAffordances } from '../../api/locations/methods.js';
 import { log } from '../../api/logs.js';
+import { Location_log } from '../../api/locations/location_log.js'
 
 
 Router.onBeforeAction(Iron.Router.bodyParser.urlencoded( {extended : false} ));
@@ -21,6 +22,19 @@ Router.route('/api/geolocation', { where: 'server' })
       lat: location.coords.latitude,
       lng: location.coords.longitude
     });
+
+    Location_log.insert({
+      uid: userId,
+      lat: location.coords.latitude,
+      lng: location.coords.longitude,
+      time: Date.now() / 1000,
+    }, (err, docs) => {
+      if (err) {
+        log.error("Not adding to location log correctly", err);
+      }
+    });
+
+
     this.response.writeHead(200, {'Content-Type': 'application/json'});
     this.response.end('ok');
   })
