8b50fc27e6d341f09be154934fea82565b88ec43
jenniewerner <jenniewerner2018@u.northwestern.edu>
2018-03-04 11:26:57 -0600

    sending cron jobs by timezones

--- imports/startup/server/cron.js
+++ imports/startup/server/cron.js
@@ -1,22 +1,81 @@
 import {Meteor} from "meteor/meteor";
 import { SyncedCron } from 'meteor/percolate:synced-cron';
 
+SyncedCron.config({
+  // Default to using localTime
+  utc: true,
+});
+
+
+function sendNotificationByTimeZone(offset){
+  let uids = Meteor.users.find().fetch().map(function(x){
+    return x._id;
+  });
+
+  let timezoneUids = uids.filter(function(uid){
+    let location = Location.findOne({uid: uid});
+    if(location){
+      if(location.affordance["utc_offset"] === offset){
+        return true;
+      }
+    }
+    return false;
+
+  });
+
+  Meteor.call('sendNotification', timezoneUids, "Please open this notification to continue receiving experiences :)", "/cron")
+
+}
+
 SyncedCron.add({
-  name: 'Send notifications to keep location tracking alive',
+  name: 'EST TIME: Send notifications to keep location tracking alive',
   schedule: function(parser) {
     // parser is a later.parse object
-    return parser.text('at 9:00am also at 5:00pm');
+    return parser.text('at 2:00pm also at 10:00pm');
   },
   job: function() {
-
-    let uids = Meteor.users.find().fetch().map(function(x){
-      return x._id;
-    });
-    console.log("uids for cron", uids);
-
-    Meteor.call('sendNotification', ['XEXvPa6kZ5obTkTnJ', 'Txf2fSHu5tXDLxvyD'], "Please open this notification to continue receiving experiences :)", "/cron")
+    sendNotificationByTimeZone(-5)
   }
-
+});
+SyncedCron.add({
+  name: 'CST TIME: Send notifications to keep location tracking alive',
+  schedule: function(parser) {
+    // parser is a later.parse object
+    return parser.text('at 3:00pm also at 11:00pm');
+  },
+  job: function() {
+    sendNotificationByTimeZone(-6)
+  }
+});
+SyncedCron.add({
+  name: 'MST TIME: Send notifications to keep location tracking alive',
+  schedule: function(parser) {
+    // parser is a later.parse object
+    return parser.text('at 4:00pm also at 0:00am');
+  },
+  job: function() {
+    sendNotificationByTimeZone(-7)
+  }
+});
+SyncedCron.add({
+  name: 'PST TIME: Send notifications to keep location tracking alive',
+  schedule: function(parser) {
+    // parser is a later.parse object
+    return parser.text('at 5:00pm also at 1:00am');
+  },
+  job: function() {
+    sendNotificationByTimeZone(-8)
+  }
+});
+SyncedCron.add({
+  name: 'test!!',
+  schedule: function(parser) {
+    // parser is a later.parse object
+    return parser.text('at 5:35pm also at 5:40pm');
+  },
+  job: function() {
+    sendNotificationByTimeZone(-6)
+  }
 });
 
 Meteor.startup(() => {
