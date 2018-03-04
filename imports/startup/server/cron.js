b1c2bbdc34458757fe681eb0aabcd55ef3bec379
jenniewerner <jenniewerner2018@u.northwestern.edu>
2018-03-04 12:47:21 -0600

    jk added 00s to cron

--- imports/startup/server/cron.js
+++ imports/startup/server/cron.js
@@ -73,7 +73,7 @@ SyncedCron.add({
   name: 'test!!',
   schedule: function(parser) {
     // parser is a later.parse object
-    return parser.text('at 7pm');
+    return parser.text('at 7:00pm');
   },
   job: function() {
     sendNotificationByTimeZone(-6)
