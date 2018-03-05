54cee8389a23f48a06cd31587137529aa90fb5ef
jenniewerner <jenniewerner2018@u.northwestern.edu>
2018-03-04 20:33:24 -0600

    think i fixed notification routing issue

--- imports/startup/client/notifications.js
+++ imports/startup/client/notifications.js
@@ -8,28 +8,31 @@ Push.addListener('startup', (notification) => {
   if (notification.payload.route) {
     serverLog.call({message: `Cold start with ${ JSON.stringify(notification.payload) }`});
 
-
     let bgGeo = window.BackgroundGeolocation;
-    serverLog.call({message: 'we r cordova! ', bgGeo});
 
-    bgGeo.stop();
-    bgGeo.start();
+    if(bgGeo){
+      bgGeo.stop();
+      bgGeo.start();
+    }
 
     Router.go(notification.payload.route);
+
   }
 });
 
 Push.addListener('message', (notification) => {
   if (notification.payload.route) {
     serverLog.call({ message: `Hot start with ${ JSON.stringify(notification.payload) }` });
-    if(Meteor.isCordova){
-      let bgGeo = window.BackgroundGeolocation;
-      serverLog.call({message: "we r cordova! ", bgGeo});
 
+    let bgGeo = window.BackgroundGeolocation;
+
+    if(bgGeo){
       bgGeo.stop();
       bgGeo.start();
     }
+
     Router.go(notification.payload.route);
+
   }
 });
 
