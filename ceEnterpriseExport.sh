fe3f30bd70c842de0ae9e3f1ff08560501b18d14
Kapil Garg <kapil1garg@gmail.com>
2018-04-26 23:44:22 -0500

    adding ios export scripts

---
+++ ceEnterpriseExport.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+xcodebuild clean \
+    -workspace ce-platform.xcworkspace/ \
+    -scheme ce-platform
+
+xcodebuild \
+    -workspace ce-platform.xcworkspace \
+    -scheme ce-platform \
+    -archivePath build/ce-platform.xcarchive \
+    archive
+
+xcodebuild \
+  -exportArchive \
+  -archivePath build/ce-platform.xcarchive \
+  -exportOptionsPlist exportOptions.plist \
+  -exportPath ce-platform-export
