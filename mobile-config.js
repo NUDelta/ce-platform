aaef1572e630dba7590f879b64061a69973e54b5
Kapil Garg <kapil1garg@gmail.com>
2018-05-14 17:30:48 -0500

    adding descriptions for camera and photo library usage

--- mobile-config.js
+++ mobile-config.js
@@ -3,14 +3,16 @@ App.accessRule('blob:*');
 App.accessRule('*.google.com/*');
 App.accessRule('*.googleapis.com/*');
 App.accessRule('*.gstatic.com/*');
+
 App.appendToConfig(`<platform name="ios">
     <config-file platform="ios" target="*-Info.plist" parent="NSPhotoLibraryUsageDescription">
-      <string>YOUR DESCRIPTION (PHOTOS PERMISSION) HERE</string>
+      <string>Collective Experiences needs photo library access to allow you to submit photos for Experiences.</string>
     </config-file>
     <config-file platform="ios" target="*-Info.plist" parent="NSCameraUsageDescription">
-      <string>YOUR DESCRIPTION (CAMERA PERMISSION) HERE</string>
+      <string>Collective Experiences needs camera access to allow you to take photos for Experiences.</string>
     </config-file>
   </platform>`);
+
 App.configurePlugin('phonegap-plugin-push', {
   SENDER_ID: 12341234
 });
