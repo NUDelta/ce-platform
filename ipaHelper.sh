ebd62be2dc5f36741019c1d8090f18499db15e53
youralien <ryanlouie2021@u.northwestern.edu>
2018-07-10 17:48:20 -0500

    new preview image, debugging camera button ui flow

--- ipaHelper.sh
+++ ipaHelper.sh
@@ -1,5 +1,5 @@
 rm -rf ../ce-platform-ios
-npm run build-dev
+npm run build
 cp ceEnterpriseExport.sh ../ce-platform-ios/ios/project
 cp exportOptions.plist ../ce-platform-ios/ios/project
 cd ../ce-platform-ios/ios/project
