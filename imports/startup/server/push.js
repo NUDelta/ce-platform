be3108a503da204f5921555c562b602155e33511
youralien <ryanlouie2021@u.northwestern.edu>
2018-07-26 00:05:22 -0500

    updated push to use prod vs dev certs/keys conditionally

--- imports/startup/server/push.js
+++ imports/startup/server/push.js
@@ -1,13 +1,24 @@
 Push.debug = true;
 
-Push.Configure({
-  apn: {
+let apn_config = (process.env.MODE === "PROD"
+  ? {
+    // --- edu.northwestern.delta.A ---
     production: true,
     keyData: Assets.getText("ios/pp.pem"),
     certData: Assets.getText("ios/cc.pem"),
     passphrase: "limo",
     gateway: "gateway.push.apple.com"
-  }
+  } : {
+    // --- edu.northwestern.delta.ce-platform ---
+    production: false,
+    keyData: Assets.getText("ios/edu.northwestern.delta.ce-platform.cert.pem"),
+    certData: Assets.getText("ios/edu.northwestern.delta.ce-platform.key.pem"),
+    passphrase: "",
+    gateway: "gateway.push.apple.com"
+  });
+
+Push.Configure({
+  apn: apn_config
 });
 
 Push.allow({
