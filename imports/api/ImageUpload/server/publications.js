dc4d1949a15179a38c39d764e86b830ab6e75ea7
youralien <ryanlouie2021@u.northwestern.edu>
2018-07-29 13:22:32 -0500

    Sort Images publication by first uploads first

--- imports/api/ImageUpload/server/publications.js
+++ imports/api/ImageUpload/server/publications.js
@@ -3,7 +3,7 @@ import { Images } from '../images.js';
 
 Meteor.publish('images.activeIncident', function (incidentId) {
   //console.log('subscribing to ImageUpload.activeIncident', incidentId);
-  return Images.find({ iid: incidentId });
+  return Images.find({ iid: incidentId }, { sort: { uploadedAt: 1 } } );
 });
 
 Meteor.publish('images.all', function () {
