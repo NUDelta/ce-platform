87a78916d9b873cb2e2001367f1ef804875717f7
youralien <ryanlouie2021@u.northwestern.edu>
2018-08-01 10:14:54 -0500

    Hacky solution to white screen of death

--- imports/startup/client/load.js
+++ imports/startup/client/load.js
@@ -5,7 +5,6 @@ import {serverLog} from "../../api/logs";
 
 
 Meteor.startup(() => {
-
   console.log("this is loading");
   GoogleMaps.load({ key: 'AIzaSyA4LczQjd7IBHE0VV-WDRo0qkkGPQJBn6I' });
 
@@ -13,4 +12,7 @@ Meteor.startup(() => {
     passwordSignupFields: 'USERNAME_ONLY'
   });
 
+  // TODO(rlouie): Hacky Solution to White Screen of Death
+  // @see https://github.com/meteor/meteor/issues/7018#issuecomment-301562610
+  Session.set('global.started', true);
 });
