bf240f2d5ad5ac05389f203713e0ab66881c9ba8
youralien <ryanlouie2021@u.northwestern.edu>
2018-08-25 05:56:57 -0500

    Cleanup based on Personal PR review

--- imports/startup/client/load.js
+++ imports/startup/client/load.js
@@ -1,5 +1,4 @@
 import { GoogleMaps } from 'meteor/dburles:google-maps';
-// import {Accounts} from "meteor/accounts-base";
 import {Meteor} from "meteor/meteor";
 import {serverLog} from "../../api/logs";
 
@@ -7,10 +6,6 @@ import {serverLog} from "../../api/logs";
 Meteor.startup(() => {
   console.log("this is loading");
   GoogleMaps.load({ key: 'AIzaSyA4LczQjd7IBHE0VV-WDRo0qkkGPQJBn6I' });
-
-  // Accounts.ui.config({
-  //   passwordSignupFields: 'USERNAME_ONLY'
-  // });
 
   // TODO(rlouie): Hacky Solution to White Screen of Death
   // @see https://github.com/meteor/meteor/issues/7018#issuecomment-301562610
