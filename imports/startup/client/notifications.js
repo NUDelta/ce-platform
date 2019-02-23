16c2f4a4489f6e062113d9d25f247356b40c6fb8
youralien <ryanlouie2021@u.northwestern.edu>
2019-02-23 15:41:07 -0600

    Add insertLog for client opens notifications

--- imports/startup/client/notifications.js
+++ imports/startup/client/notifications.js
@@ -1,6 +1,7 @@
 import { Push } from 'meteor/raix:push';
 import { Router } from 'meteor/iron:router';
 import { log, serverLog } from '../../api/logs.js';
+import {Meteor} from "meteor/meteor";
 
 Push.Configure({
   android: {
@@ -32,6 +33,19 @@ Push.addListener('startup', (notification) => {
       bgGeo.start();
     }
 
+    if (Meteor.userId()) {
+      let dic = {
+        uid: Meteor.userId(),
+        timestamp: Date.now(),
+        route: "opennotification",
+        params: {
+          pushListener: 'startup',
+          payload: notification.payload
+        }
+      };
+      Meteor.call('insertLog', dic);
+    }
+
     Router.go(notification.payload.route);
   }
 });
@@ -45,6 +59,19 @@ Push.addListener('message', (notification) => {
     if(bgGeo){
       bgGeo.stop();
       bgGeo.start();
+    }
+
+    if (Meteor.userId()) {
+      let dic = {
+        uid: Meteor.userId(),
+        timestamp: Date.now(),
+        route: "opennotification",
+        params: {
+          pushListener: 'message',
+          payload: notification.payload
+        }
+      };
+      Meteor.call('insertLog', dic);
     }
 
     Router.go(notification.payload.route);
