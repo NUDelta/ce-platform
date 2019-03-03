f75072bc5ee7a1c5c69158e97ba94b2a28b83138
youralien <ryanlouie2021@u.northwestern.edu>
2019-03-03 14:34:32 -0600

    Reactive Map UI + time of last location update

--- imports/ui/pages/affordances.js
+++ imports/ui/pages/affordances.js
@@ -1,6 +1,7 @@
 import './affordances.html';
 
 import { Meteor } from 'meteor/meteor';
+import { Tracker } from 'meteor/tracker';
 import { Template } from 'meteor/templating';
 
 import { Locations } from '../../api/UserMonitor/locations/locations.js';
@@ -9,10 +10,27 @@ import { GoogleMaps } from 'meteor/dburles:google-maps';
 Template.affordances.onCreated(function () {
 
   GoogleMaps.ready('yourLocation', function (map) {
-    // Add a marker to the map once it's ready
-    var marker = new google.maps.Marker({
-      position: map.options.center,
-      map: map.instance
+    let marker;
+
+    // Create and move the marker when latLng changes.
+    Tracker.autorun(function() {
+      if (!this.location)
+        return;
+
+      // If the marker doesn't yet exist, create it.
+      if (!marker) {
+        marker = new google.maps.Marker({
+          position: new google.maps.LatLng(this.location.lat, this.location.lng),
+          map: map.instance
+        });
+      }
+      // The marker already exists, so we'll just change its position.
+      else {
+        marker.setPosition({'lat': this.location.lat, 'lng': this.location.lng});
+      }
+
+      // Center the map view onto the current position.
+      map.instance.setCenter(marker.getPosition());
     });
   });
 
@@ -40,6 +58,26 @@ Template.affordances.helpers({
         center: new google.maps.LatLng(location.lat, location.lng),
         zoom: 18
       };
+    }
+  },
+  timeSinceLastLocation() {
+    let timediff_seconds = Math.floor((Date.now() - this.location.time) / 1000);
+    // 1 minute
+    if (timediff_seconds < 60) {
+      return timediff_seconds + " seconds";
+    }
+    // 60 minutes
+    else if (timediff_seconds < 3600) {
+      let timediff_min = Math.floor(timediff_seconds / 60);
+      return timediff_min + " minutes";
+    }
+    // 24 hours
+    else if (timediff_seconds < 86400) {
+      let timediff_hour = Math.floor(timediff_seconds / (3600));
+      return timediff_hour + " hours";
+    } else {
+      let timediff_days = Math.floor(timediff_seconds / (86400));
+      return timediff_days + " days";
     }
   },
   affordanceKeys() {
