c3baf6bf22d6db5910633302aef8c70328d5545d
youralien <ryanlouie2021@u.northwestern.edu>
2019-03-08 11:07:30 -0600

    Added logging for decommissions

---
+++ imports/api/Logging/decommission_log.js
@@ -0,0 +1,61 @@
+import { Mongo } from 'meteor/mongo';
+import { SimpleSchema } from 'meteor/aldeed:simple-schema';
+import { Schema } from '../schema.js';
+
+export const Decommission_log = new Mongo.Collection('decommission_log');
+
+Schema.Decommission_log = new SimpleSchema({
+  iid: {
+    type: String,
+    regEx: SimpleSchema.RegEx.Id,
+    label: 'incident id that user is being removed from'
+  },
+  uid: {
+    type: String,
+    regEx: SimpleSchema.RegEx.Id,
+    label: 'user id'
+  },
+  needName: {
+    type: String,
+    label: 'need name that user is being removed from'
+  },
+  lat: {
+    type: Number,
+    decimal: true,
+    min: -90,
+    max: 90,
+    label: 'latitude: last estimated location when decommissioned'
+  },
+  lng: {
+    type: Number,
+    decimal: true,
+    min: -180,
+    max: 180,
+    label: 'longitude: last estimated location when decommissioned'
+  },
+  timestamp: {
+    type: Date,
+  },
+  needMatch: {
+    type: Boolean
+  },
+  affordances: {
+    type: Object,
+    blackbox: true,
+    optional: true
+  }
+});
+
+Decommission_log.attachSchema(Schema.Decommission_log);
+
+Decommission_log.allow({
+  insert: function () {
+    return true;
+  },
+  update: function () {
+    return true;
+  },
+  remove: function () {
+    return true;
+  }
+});
