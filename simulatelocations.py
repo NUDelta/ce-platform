a8a1e7f89d632b2e58b09b3c4553b3717f7df308
youralien <ryanlouie2021@u.northwestern.edu>
2019-05-30 17:07:33 -0500

    updated simulatelocations.py, tabs -> spaces

--- simulatelocations.py
+++ simulatelocations.py
@@ -16,24 +16,24 @@ brisbane = (-37.822464, 144.966146)
 library = (42.058141, -87.674490)
 
 def followPath(path, uid):
-	for stop in path:
-	    setLocation(stop, uid)
-	    time.sleep(1)
+    for stop in path:
+        setLocation(stop, uid)
+        time.sleep(1)
 
 def setLocation(location, uid):
-        host = "http://localhost:3000" 
-        # host = "https://ce-platform.herokuapp.com"
-        # host = "https://staging-ce-platform.herokuapp.com"
-	r = requests.post(host + "/api/geolocation", json={
-	            "userId": uid,
-	            "location": {
-	                "coords": {
+    host = "http://localhost:3000"
+    # host = "https://ce-platform.herokuapp.com"
+    # host = "https://staging-ce-platform.herokuapp.com"
+    r = requests.post(host + "/api/geolocation", json={
+                "userId": uid,
+                "location": {
+                    "coords": {
                         "latitude": location[0],
                         "longitude": location[1]
-	                },
-	                "activity": {"type": "unknown", "confidence": 100}
+                    },
+                    "activity": {"type": "unknown", "confidence": 100}
                 }
-	        })
+            })
     #print(uid + "at location " + str(location[0]) + " " + str(location[1]))
 
 
@@ -44,81 +44,81 @@ def setLocation(location, uid):
 #This test checks that users receive experiences when they get an affordance
 #   and then loose the correct experiences when they loose the affordance
 def basicUserMovement():
-	print("Log in as all users")
-	#time.sleep(10)
-	setLocation(park, sys.argv[1])
-	print("User A should now have two experiences, participate in scavenger hunt")
-	print("after participating, user A should only have restaurant!")
+    print("Log in as all users")
+    #time.sleep(10)
+    setLocation(park, sys.argv[1])
+    print("User A should now have two experiences, participate in scavenger hunt")
+    print("after participating, user A should only have restaurant!")
 
-	time.sleep(20)
-	setLocation(park, sys.argv[2])
-	print("user B should now have two experiences")
+    time.sleep(20)
+    setLocation(park, sys.argv[2])
+    print("user B should now have two experiences")
 
-	time.sleep(20)
-	setLocation(burgers, sys.argv[2])
-	print("user B should now have one experience")
+    time.sleep(20)
+    setLocation(burgers, sys.argv[2])
+    print("user B should now have one experience")
 
 def threeUsersMoving():
-	print("Log in as all users")
-	time.sleep(5)
-	setLocation(park, sys.argv[1])
-	print("User A should now have two experiences")
+    print("Log in as all users")
+    time.sleep(5)
+    setLocation(park, sys.argv[1])
+    print("User A should now have two experiences")
 
-	time.sleep(20)
-	setLocation(park, sys.argv[2])
-	print("user B should now have two experiences")
+    time.sleep(20)
+    setLocation(park, sys.argv[2])
+    print("user B should now have two experiences")
 
-	time.sleep(20)
-	setLocation(burgers, sys.argv[2])
-	print("user B should now have one experience")
+    time.sleep(20)
+    setLocation(burgers, sys.argv[2])
+    print("user B should now have one experience")
 
-	time.sleep(20)
-	setLocation(park, sys.argv[3])
-	print("user C should now have two experiences")
+    time.sleep(20)
+    setLocation(park, sys.argv[3])
+    print("user C should now have two experiences")
 
-	time.sleep(20)
-	setLocation(park, sys.argv[2])
-	print("user B changes locations but should not gain experiences")
+    time.sleep(20)
+    setLocation(park, sys.argv[2])
+    print("user B changes locations but should not gain experiences")
 
 def userWaitsBetweenNotifications():
-	print("Log in as all users")
-	time.sleep(5)
-	setLocation(park, sys.argv[1])
-	print("User A should now have two experiences")
-	print("Participate in one experience")
-	time.sleep(20)
-	print("participation should be done by now")
-	print("User should not regain experiences")
-	setLocation(park, sys.argv[1])
+    print("Log in as all users")
+    time.sleep(5)
+    setLocation(park, sys.argv[1])
+    print("User A should now have two experiences")
+    print("Participate in one experience")
+    time.sleep(20)
+    print("participation should be done by now")
+    print("User should not regain experiences")
+    setLocation(park, sys.argv[1])
 
-	time.sleep(50)
-	setLocation(park, sys.argv[1])
-	print("User should regain experience!")
+    time.sleep(50)
+    setLocation(park, sys.argv[1])
+    print("User should regain experience!")
 
 def threeUsersParticipateInExperiences():
-	print("Log in as all users")
-	time.sleep(5)
-	setLocation(park, sys.argv[1])
-	# time.sleep(10)
+    print("Log in as all users")
+    time.sleep(5)
+    setLocation(park, sys.argv[1])
+    # time.sleep(10)
 
-	setLocation(park, sys.argv[2])
-	# time.sleep(20)
+    setLocation(park, sys.argv[2])
+    # time.sleep(20)
 
-	# setLocation(park, sys.argv[3])
-	# print("User C should now have one experiences")
-	# time.sleep(10)
+    # setLocation(park, sys.argv[3])
+    # print("User C should now have one experiences")
+    # time.sleep(10)
 
-	# setLocation(park, sys.argv[1])
-	# print("User A should have one experiences")
-	# print("Participate in restaurant")
-	# time.sleep(10)
+    # setLocation(park, sys.argv[1])
+    # print("User A should have one experiences")
+    # print("Participate in restaurant")
+    # time.sleep(10)
 
-	# setLocation(burgers, sys.argv[2])
-	# print("User B should have no experiences")
-	# time.sleep(5)
+    # setLocation(burgers, sys.argv[2])
+    # print("User B should have no experiences")
+    # time.sleep(5)
 
-	setLocation(burgers, sys.argv[3])
-	# time.sleep(10)
+    setLocation(burgers, sys.argv[3])
+    # time.sleep(10)
 
 def oneUserMoving():
     print("Log in as user a")
@@ -147,57 +147,57 @@ def oneUserMoving():
     print("slight movement")
 
 def test5():
-	while(True):
-		print("josh goes to the train")
-		setLocation(train, sys.argv[5])
-		time.sleep(15)
+    while(True):
+        print("josh goes to the train")
+        setLocation(train, sys.argv[5])
+        time.sleep(15)
 
-		print("garrett gets to the grocery store")
-		setLocation(grocery, sys.argv[1])
-		print("meg gets to the bar")
-		setLocation(beer, sys.argv[3])
-		time.sleep(15)
+        print("garrett gets to the grocery store")
+        setLocation(grocery, sys.argv[1])
+        print("meg gets to the bar")
+        setLocation(beer, sys.argv[3])
+        time.sleep(15)
 
-		print("garretts brother gets to the grocery store")
-		setLocation(grocery, sys.argv[2])
-		time.sleep(15)
+        print("garretts brother gets to the grocery store")
+        setLocation(grocery, sys.argv[2])
+        time.sleep(15)
 
-		print("megs sister gets to the bar")
-		setLocation(beer, sys.argv[4])
-		time.sleep(15)
+        print("megs sister gets to the bar")
+        setLocation(beer, sys.argv[4])
+        time.sleep(15)
 
-		print("josh gets to the park")
-		setLocation(park, sys.argv[5])
-		time.sleep(15)
+        print("josh gets to the park")
+        setLocation(park, sys.argv[5])
+        time.sleep(15)
 
-		print("meg and her sister goes to the train")
-		setLocation(train, sys.argv[3])
-		setLocation(train, sys.argv[4])
-		time.sleep(15)
+        print("meg and her sister goes to the train")
+        setLocation(train, sys.argv[3])
+        setLocation(train, sys.argv[4])
+        time.sleep(15)
 
-		print("garrett goes to a park")
-		setLocation(park, sys.argv[1])
-		time.sleep(15)
+        print("garrett goes to a park")
+        setLocation(park, sys.argv[1])
+        time.sleep(15)
 
-		print("garrett goes to the train")
-		setLocation(train, sys.argv[1])
-		time.sleep(15)
+        print("garrett goes to the train")
+        setLocation(train, sys.argv[1])
+        time.sleep(15)
 
 def allUsersAtPark():
-	setLocation(park, sys.argv[1])
-	setLocation(park, sys.argv[2])
-	setLocation(park, sys.argv[3])
-	setLocation(park, sys.argv[4])
-	setLocation(park, sys.argv[5])
-	print("all usrs at parks")
+    setLocation(park, sys.argv[1])
+    setLocation(park, sys.argv[2])
+    setLocation(park, sys.argv[3])
+    setLocation(park, sys.argv[4])
+    setLocation(park, sys.argv[5])
+    print("all usrs at parks")
 
 def allUsersAtCastle():
-	setLocation((42.050538,-87.677355), sys.argv[1])
-	setLocation((42.050538,-87.677355), sys.argv[2])
-	setLocation((42.050538,-87.677355), sys.argv[3])
-	setLocation((42.050538,-87.677355), sys.argv[4])
-	setLocation((42.050538,-87.677355), sys.argv[5])
-	print("all usrs at castle")
+    setLocation((42.050538,-87.677355), sys.argv[1])
+    setLocation((42.050538,-87.677355), sys.argv[2])
+    setLocation((42.050538,-87.677355), sys.argv[3])
+    setLocation((42.050538,-87.677355), sys.argv[4])
+    setLocation((42.050538,-87.677355), sys.argv[5])
+    print("all usrs at castle")
 
 def allUsersAtRestaurant():
     setLocation(burgers, sys.argv[1])
@@ -207,31 +207,31 @@ def allUsersAtRestaurant():
 
 
 def allUsersAtGrocery():
-	setLocation(grocery, sys.argv[1])
-	setLocation(grocery, sys.argv[2])
-	setLocation(grocery, sys.argv[3])
-	setLocation(grocery, sys.argv[3])
-	setLocation(grocery, sys.argv[3])
+    setLocation(grocery, sys.argv[1])
+    setLocation(grocery, sys.argv[2])
+    setLocation(grocery, sys.argv[3])
+    setLocation(grocery, sys.argv[3])
+    setLocation(grocery, sys.argv[3])
 
-	print("all users at grocery")
+    print("all users at grocery")
 
 def allUsersAtBars():
-	setLocation((42.046251, -87.680547), sys.argv[1])
-	setLocation((42.046251, -87.680547), sys.argv[2])
-	setLocation((42.046251, -87.680547), sys.argv[3])
-	setLocation((42.046251, -87.680547), sys.argv[4])
-	setLocation((42.046251, -87.680547), sys.argv[5])
+    setLocation((42.046251, -87.680547), sys.argv[1])
+    setLocation((42.046251, -87.680547), sys.argv[2])
+    setLocation((42.046251, -87.680547), sys.argv[3])
+    setLocation((42.046251, -87.680547), sys.argv[4])
+    setLocation((42.046251, -87.680547), sys.argv[5])
 
-	print("all users at bar")
+    print("all users at bar")
 
 def allUsersAtTrain():
-	setLocation(train, sys.argv[1])
-	setLocation(train, sys.argv[2])
-	setLocation(train, sys.argv[3])
-	setLocation(train, sys.argv[4])
-	setLocation(train, sys.argv[5])
+    setLocation(train, sys.argv[1])
+    setLocation(train, sys.argv[2])
+    setLocation(train, sys.argv[3])
+    setLocation(train, sys.argv[4])
+    setLocation(train, sys.argv[5])
 
-	print("all users at train")
+    print("all users at train")
 
 def userAatCoffee():
     setLocation(coffee, sys.argv[1])
@@ -243,40 +243,40 @@ def userAatGrocery():
     setLocation(grocery, sys.argv[1])
 
 def allUsersCalifornia():
-	setLocation((34, -120), sys.argv[1])
-	setLocation((34, -120), sys.argv[2])
-	setLocation((34, -120), sys.argv[3])
-	setLocation((34, -120), sys.argv[4])
-	setLocation((34, -120), sys.argv[5])
+    setLocation((34, -120), sys.argv[1])
+    setLocation((34, -120), sys.argv[2])
+    setLocation((34, -120), sys.argv[3])
+    setLocation((34, -120), sys.argv[4])
+    setLocation((34, -120), sys.argv[5])
 
-	print("all users in CA")
+    print("all users in CA")
 
 def allUsersSyd():
-	setLocation(sydney, sys.argv[1])
-	setLocation(sydney, sys.argv[2])
-	setLocation(sydney, sys.argv[3])
-	setLocation(sydney, sys.argv[4])
-	setLocation(sydney, sys.argv[5])
+    setLocation(sydney, sys.argv[1])
+    setLocation(sydney, sys.argv[2])
+    setLocation(sydney, sys.argv[3])
+    setLocation(sydney, sys.argv[4])
+    setLocation(sydney, sys.argv[5])
 
-	print("all users in sydney")
+    print("all users in sydney")
 
 def allUsersLakefill():
-	setLocation((42.052460,-87.669876), sys.argv[1])
-	setLocation((42.052460,-87.669876), sys.argv[2])
-	setLocation((42.052460,-87.669876), sys.argv[3])
-	setLocation((42.052460,-87.669876), sys.argv[4])
-	setLocation((42.052460,-87.669876), sys.argv[5])
+    setLocation((42.052460,-87.669876), sys.argv[1])
+    setLocation((42.052460,-87.669876), sys.argv[2])
+    setLocation((42.052460,-87.669876), sys.argv[3])
+    setLocation((42.052460,-87.669876), sys.argv[4])
+    setLocation((42.052460,-87.669876), sys.argv[5])
 
-	print("all users in lakefill")
+    print("all users in lakefill")
 
 def onlyGarretAtLake():
-	setLocation((42.052460,-87.669876), sys.argv[1])
-	setLocation(train, sys.argv[2])
-	setLocation(train, sys.argv[3])
-	setLocation(train, sys.argv[4])
-	setLocation(train, sys.argv[5])
+    setLocation((42.052460,-87.669876), sys.argv[1])
+    setLocation(train, sys.argv[2])
+    setLocation(train, sys.argv[3])
+    setLocation(train, sys.argv[4])
+    setLocation(train, sys.argv[5])
 
-	print("only garrett at lakefill")
+    print("only garrett at lakefill")
 
 def garrettAndMegBump():
     setLocation(grocery, sys.argv[1])
@@ -300,6 +300,7 @@ if __name__ == "__main__":
     # single user movement
     # setLocation(burgers, sys.argv[1])
     # allUsersGrocery()
+    # setLocation(library, "TKS3kFCMZTZZa9S82")
     #garrettAndMegBump()
     #time.sleep(2)
     #onlyGarretAtLake()
