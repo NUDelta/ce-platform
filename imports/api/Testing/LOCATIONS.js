7613f2e79db9b800cadd8f03d7c1fd30093b6ab0
youralien <ryanlouie2021@u.northwestern.edu>
2020-03-08 16:04:01 -0500

    Refactored DETECTORS, LOCATIONS, USERS, and experience helper functions into separate files

---
+++ imports/api/Testing/LOCATIONS.js
@@ -0,0 +1,26 @@
+export let LOCATIONS = {
+  'park': {
+    lat: 42.056838,
+    lng: -87.675940
+  },
+  'lakefill': {
+    lat: 42.054902,
+    lng: -87.670197
+  },
+  'burgers': {
+    lat: 42.046131,
+    lng: -87.681559
+  },
+  'grocery': {
+    lat: 42.047621,
+    lng: -87.679488
+  },
+  'sushi': { // sashimi sashimi near whole foods
+    lat: 42.048068,
+    lng: -87.6811262
+  },
+  'grocery2': {
+    lat: 42.039818,
+    lng: -87.680088
+  }
+};
