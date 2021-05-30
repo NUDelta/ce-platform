64f67ba9020493e8fec3018e41499c1ca4ad1595
Kevin Cheng <kevincheng2021@u.northwestern.edu>
2021-05-30 14:53:16 -0500

    Fixed expanded page to be fully functional

--- imports/ui/pages/api_custom_results_expand.js
+++ imports/ui/pages/api_custom_results_expand.js
@@ -35,17 +35,48 @@ Template.api_custom_results_expand.helpers({
 
 Template.seniorFinalsExpand.helpers({
   getSub(){
-    console.log('running')
+    // console.log('running')
     const subId = Router.current().params.sid;
     const thisSub = this.submissions.filter(function(x){
-      return x._id = subId;
+      // console.log('id ', subId);
+      if (x._id === subId) {
+        return x;
+      }
+      // return x._id = subId;
     })
-    console.log('getsub',thisSub);
+    // console.log('getsub ',thisSub);
     return thisSub;
+  },
+  getImageSub(sub) {
+    creatorSub = this.images.find(i => i._id === sub.content.proof);
+    return creatorSub;
+  },
+  getUsername(sub){
+    let fullname = this.users.filter(function(user){
+      if (user._id === sub.uid) {
+        return user;
+      }
+    });
+
+    return fullname[0].profile.firstName + " " + fullname[0].profile.lastName;
+  },
+  getTimeStamp(sub){
+    let timeBlocks = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    console.log('time ',sub.timestamp.getDay(0));
+    let dayOfWeek = timeBlocks[sub.timestamp.getDay()];
+    return dayOfWeek + ", " + sub.timestamp.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'});
+  },
+  getSentence(sub){
+    return "\"" + sub.content.sentence + "\"";
   }
 });
 
 //note: needName for triparticipate often split into needName_triad#
 Template.api_custom_results_expand.events({
   //Todo: a back button?
+  'click #back-btn': function(event, instance) {
+    // console.log('back');
+    // event.preventDefault();
+    window.history.back();
+  }
 });
