c9380e48c2a6502c645a4080268049c4c481bebb
Mason Lin <Mason@Masons-MBP-2.attlocal.net>
2021-06-07 13:38:42 -0700

    Revert "adding detector variables"
    This reverts commit e87d1e3f9a8393adb487ca81ecb8812280b2e1fb.

--- imports/api/Testing/DETECTORS.js
+++ imports/api/Testing/DETECTORS.js
@@ -141,55 +141,33 @@ export let DETECTORS = {
     _id: Random.id(),
     description: 'Library Triad 1',
     variables: [
-      'var libraries;',
-      'var usedbooks;',
-      'var bookstores;'
+      'var participatedInGroupCheers',
+      'var triad1',
+      'var daytime;'
     ],
-    rules: ['(libraries || bookstores);']
+    rules: ['(triad1 && daytime)']
   },
 
   groceries_triad1:{
     _id: Random.id(),
     description: 'Groceries Triad 1',
     variables: [
-      'var intlgrocery;',
-      'var ethicgrocery;',
-      'var markets;',
-      'var wholesalers;',
-      'var pharmacy;',
-      'var grocery;',
-      'var farmersmarket;',
-      'var convenience;',
-      'var importedfood;',
-      'var herbsandspices;',
-      'var drugstores;',
-      'var seafoodmarkets;',
-      'var marketstalls;',
-      'var organic_stores;',
-      'var publicmarkets;'
+      'var participatedInGroupCheers',
+      'var triad1',
+      'var daytime;'
     ],
-    rules: ['(intlgrocery || ethicgrocery) || ((markets || wholesalers) || ((pharmacy || grocery) || ((farmersmarket || convenience) || ((importedfood || herbsandspices) || ((drugstores || seafoodmarkets) || ((organic_stores || publicmarkets) || marketstalls))))));']
+    rules: ['(triad1 && daytime)']
   },
 
   restaurant_triad1:{
     _id: Random.id(),
     description: 'Restaurant Triad 1',
     variables: [
+      'var participatedInGroupCheers',
       'var triad1',
-      'var daytime;',
-      'var triad1',
-      'var coffeeroasteries;',
-      'var coffee;',
-      'var cafes;',
-      'var coffeeshops;',
-      'var coffeeteasupplies;',
-      'var diners;',
-      'var restaurants;',
-      'var cafeteria;',
-      'var food_court;',
-      'var bars;',
+      'var daytime;'
     ],
-    rules: ['(triad1 && daytime && (coffeeroasteries || coffee || coffeeshops || coffeeteasupplies || cafes || diners || restaurants || cafeteria || food_court || bars));']
+    rules: ['(triad1 && daytime)']
   },
 
   //  /////////////////////////////////////////////////////////
