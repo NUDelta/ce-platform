dc92fc4c01fad85ac88af4c6b562071a7bb46c77
youralien <ryanlouie2021@u.northwestern.edu>
2021-11-08 14:46:42 -0600

    Support new P8 keys for apple push notification
    Summary
    - Upgrade package nudelta2015:push to 0.0.3 to support passing P8 keys
    - Add P8 files for 2021-2022 year
    - link provider options in push.js

--- imports/startup/server/push.js
+++ imports/startup/server/push.js
@@ -5,7 +5,12 @@ Push.Configure({
     production: true,
     // keyData: Assets.getText("ios/pp.pem"), // depreciated for nudelta2015:push
     // certData: Assets.getText("ios/cc.pem"), // depreciated for nudelta2015:push
-    pfx: Assets.absoluteFilePath("ios/edu.northwestern.delta.D.p12"),
+    // pfx: Assets.absoluteFilePath("ios/edu.northwestern.delta.D.p12"), // depreciated, use P8
+    token: {
+      key: Assets.absoluteFilePath("ios/AuthKey_W9H43BKTXS.p8"),
+      keyId: "W9H43BKTXS",
+      teamId: "823S57WQK3"
+    },
     passphrase: "password",
     gateway: "gateway.push.apple.com"
   }
