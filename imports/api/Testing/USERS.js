d3eac8515bac266fa11e26263661f687e52d4ecb
Yvan Chu <yvan@u.northwestern.edu>
2022-01-20 21:58:55 -0600

    created python script for generating user accounts to GeneratedUSER.js

--- imports/api/Testing/USERS.js
+++ imports/api/Testing/USERS.js
@@ -1,180 +1,173 @@
 //previous copy is in triadic_experience
 export let USERS = {
-    user1:{
-      username: 'jennifer',
-      email: 'jennifer@email.com',
-      password: 'password',
-      profile: {
-        firstName: 'Jennifer',
-        lastName: '',
-        staticAffordances: {  //why do the staticAffordances here not showing up?
-        }
-      }
-    },
-    user2:{
-      username: 'sofia',
-      email: 'sofia@email.com',
-      password: 'password',
-      profile: {
-        firstName: 'Sofia',
-        lastName: '',
-        staticAffordances: {  //why do the staticAffordances here not showing up?
-        }
-      }
-    },
-    user3:{
-      username: 'evan',
-      email: 'evan@email.com',
-      password: 'password',
-      profile: {
-        firstName: 'Evan',
-        lastName: '',
-        staticAffordances: {  //why do the staticAffordances here not showing up?
-        }
-      }
-    },
-    user4:{
-      username: 'derex',
-      email: 'derex@email.com',
-      password: 'password',
-      profile: {
-        firstName: 'Derex',
-        lastName: '',
-        staticAffordances: {  //why do the staticAffordances here not showing up?
-        }
-      }
-    },
-  ryan: {
-    username: 'ryan',
-    email: 'ryan@email.com',
-    password: 'password',
+  user1: {
+    username: "jennifer",
+    email: "jennifer@email.com",
+    password: "password",
     profile: {
-      firstName: 'Ryan',
-      lastName: 'Louie',
-      staticAffordances: {  //why do the staticAffordances here not showing up?
-      }
-    }
+      firstName: "Jennifer",
+      lastName: "",
+      staticAffordances: {
+        //why do the staticAffordances here not showing up?
+      },
+    },
+  },
+  user2: {
+    username: "sofia",
+    email: "sofia@email.com",
+    password: "password",
+    profile: {
+      firstName: "Sofia",
+      lastName: "",
+      staticAffordances: {
+        //why do the staticAffordances here not showing up?
+      },
+    },
+  },
+  user3: {
+    username: "evan",
+    email: "evan@email.com",
+    password: "password",
+    profile: {
+      firstName: "Evan",
+      lastName: "",
+      staticAffordances: {
+        //why do the staticAffordances here not showing up?
+      },
+    },
+  },
+  user4: {
+    username: "derex",
+    email: "derex@email.com",
+    password: "password",
+    profile: {
+      firstName: "Derex",
+      lastName: "",
+      staticAffordances: {
+        //why do the staticAffordances here not showing up?
+      },
+    },
+  },
+  ryan: {
+    username: "ryan",
+    email: "ryan@email.com",
+    password: "password",
+    profile: {
+      firstName: "Ryan",
+      lastName: "Louie",
+      staticAffordances: {
+        //why do the staticAffordances here not showing up?
+      },
+    },
   },
   jenny: {
-    username: 'jenny',
-    email: 'jenny@email.com',
-    password: 'password',
+    username: "jenny",
+    email: "jenny@email.com",
+    password: "password",
     profile: {
-      firstName: 'Jenny',
-      lastName: 'Chang',
-      staticAffordances: {
-      }
-    }
+      firstName: "Jenny",
+      lastName: "Chang",
+      staticAffordances: {},
+    },
   },
   mason: {
-    username: 'mason',
-    email: 'mason@email.com',
-    password: 'password',
+    username: "mason",
+    email: "mason@email.com",
+    password: "password",
     profile: {
-      firstName: 'Mason',
-      lastName: 'Lin',
-      staticAffordances: { 
-      }
-    }
+      firstName: "Mason",
+      lastName: "Lin",
+      staticAffordances: {},
+    },
   },
   cindy: {
-    username: 'cindy',
-    email: 'cindy@email.com',
-    password: 'password',
+    username: "cindy",
+    email: "cindy@email.com",
+    password: "password",
     profile: {
-      firstName: 'Cindy',
-      lastName: 'Hu',
-      staticAffordances: {
-      }
-    }
+      firstName: "Cindy",
+      lastName: "Hu",
+      staticAffordances: {},
+    },
   },
   garret: {
-    username: 'garret',
-    email: 'garret@email.com',
-    password: 'password',
+    username: "garret",
+    email: "garret@email.com",
+    password: "password",
     profile: {
-      firstName: 'Garret', //Garret
-      lastName: 'Hedman', //hedman
-      staticAffordances: {
-      }
-    }
+      firstName: "Garret", //Garret
+      lastName: "Hedman", //hedman
+      staticAffordances: {},
+    },
   },
   fardeem: {
-    username: 'fardeem',
-    email: 'fardeem@email.com',
-    password: 'password',
+    username: "fardeem",
+    email: "fardeem@email.com",
+    password: "password",
     profile: {
-      firstName: 'Fardeem', //fardeem
-      lastName: 'Munir', //munir
-      staticAffordances: {
-      }
-    }
+      firstName: "Fardeem", //fardeem
+      lastName: "Munir", //munir
+      staticAffordances: {},
+    },
   },
   connie: {
-    username: 'connie',
-    email: 'connie@email.com',
-    password: 'password',
+    username: "connie",
+    email: "connie@email.com",
+    password: "password",
     profile: {
-      firstName: 'Connie', //connie
-      lastName: 'Chau', //chau
-      staticAffordances: {
-      }
-    }
+      firstName: "Connie", //connie
+      lastName: "Chau", //chau
+      staticAffordances: {},
+    },
   },
   roxy: {
-    username: 'roxy',
-    email: 'roxy@email.com',
-    password: 'password',
+    username: "roxy",
+    email: "roxy@email.com",
+    password: "password",
     profile: {
-      firstName: 'Roxy',
-      lastName: 'Wilcox',
-      staticAffordances: {
-      }
-    }
+      firstName: "Roxy",
+      lastName: "Wilcox",
+      staticAffordances: {},
+    },
   },
   shubhanshi: {
-    username: 'shubhanshi',
-    email: 'shubhanshi@email.com',
-    password: 'password',
+    username: "shubhanshi",
+    email: "shubhanshi@email.com",
+    password: "password",
     profile: {
-      firstName: 'Shubhanshi',
-      lastName: 'Gaudani',
-      staticAffordances: {
-      }
-    }
+      firstName: "Shubhanshi",
+      lastName: "Gaudani",
+      staticAffordances: {},
+    },
   },
   jonathan: {
-    username: 'jonathan',
-    email: 'jonathan@email.com',
-    password: 'password',
+    username: "jonathan",
+    email: "jonathan@email.com",
+    password: "password",
     profile: {
-      firstName: 'Jonathan',
-      lastName: 'Liu',
-      staticAffordances: {
-      }
-    }
+      firstName: "Jonathan",
+      lastName: "Liu",
+      staticAffordances: {},
+    },
   },
   evey: {
-    username: 'evey',
-    email: 'evey@email.com',
-    password: 'password',
+    username: "evey",
+    email: "evey@email.com",
+    password: "password",
     profile: {
-      firstName: 'Evey',
-      lastName: 'Huang',
-      staticAffordances: {
-      }
-    }
+      firstName: "Evey",
+      lastName: "Huang",
+      staticAffordances: {},
+    },
   },
   yvan: {
-    username: 'yvan',
-    email: 'yvan@email.com',
-    password: 'password',
+    username: "yvan",
+    email: "yvan@email.com",
+    password: "password",
     profile: {
-      firstName: 'Yvan',
-      lastName: 'Chu',
-      staticAffordances: {
-      }
-    }
-  }
-
+      firstName: "Yvan",
+      lastName: "Chu",
+      staticAffordances: {},
+    },
+  },
 };
