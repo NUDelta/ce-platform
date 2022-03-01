cdd5c6e3f850e7ae4ff856a6b663184978ce8033
Yvan Chu <60163603+yvanchu@users.noreply.github.com>
2022-03-01 15:41:54 -0600

    rendered prompt title and question in response accordion

--- imports/ui/react-component/ce_response.js
+++ imports/ui/react-component/ce_response.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from "react";
 import {
   Accordion,
   AccordionSummary,
@@ -7,34 +7,61 @@ import {
   Card,
   CardContent,
   CardMedia,
-} from '@mui/material';
+  Zoom,
+} from "@mui/material";
 
-import { useTracker } from 'meteor/react-meteor-data';
+import { useTracker } from "meteor/react-meteor-data";
 import { Submissions } from "../../api/OCEManager/currentNeeds.js";
+import { Experiences } from "../../api/OCEManager/OCEs/experiences.js";
 
+//TODO: pass in the prompt itself
 export const CEResponse = ({ ids }) => {
   eid = ids[0];
   iid = ids[1];
   console.log("in CEResponse");
 
+  // const [zoom1, setZoom1] = useState(false);
+  // const [zoom2, setZoom2] = useState(false);
+
+  const toggle = (state, setState) => {
+    if (state) {
+      setState(false);
+    } else {
+      setState(true);
+    }
+  };
+
   const [subHandle, submissions] = useTracker(() => {
-    const handle = Meteor.subscribe('submissions.all');
+    const handle = Meteor.subscribe("submissions.all");
     return [handle, Submissions.find({}).fetch()];
   });
 
   const [userHandle, users] = useTracker(() => {
-    const handle = Meteor.subscribe('users.all');
-    return [handle, Meteor.users.find({}).fetch()]
+    const handle = Meteor.subscribe("users.all");
+    return [handle, Meteor.users.find({}).fetch()];
   });
 
-  if (subHandle.ready() && userHandle.ready()) {
-    const mySub = submissions.find(s => (s.uid === Meteor.userId() && s.eid === eid));
-    const myNeedNames = mySub.needName;
-    const otherSubs = submissions.filter(s => myNeedNames.includes(s.needName) && s.uid !== Meteor.userId())[0];
+  const [expHandle, experiences] = useTracker(() => {
+    const handle = Meteor.subscribe("experiences.all");
+    return [handle, Experiences.find({}).fetch()];
+  });
 
-    const myImage =  mySub.content.proof;
-    const otherImages =  otherSubs.content.proof;
-    const friends = users.find(u => u._id === otherSubs.uid);
+  if (subHandle.ready() && userHandle.ready() && expHandle.ready()) {
+    const mySub = submissions.find(
+      (s) => s.uid === Meteor.userId() && s.eid === eid
+    );
+    const myNeedNames = mySub.needName;
+    const otherSubs = submissions.filter(
+      (s) => myNeedNames.includes(s.needName) && s.uid !== Meteor.userId()
+    )[0];
+
+    const myImage = mySub.content.proof;
+    const otherImages = otherSubs.content.proof;
+    const friends = users.find((u) => u._id === otherSubs.uid);
+
+    //to find information on the experience
+    const experiencePrompt = experiences.find((e) => e._id === eid);
+    console.log(experiencePrompt);
 
     results = {
       friendOneName: `${friends.profile.firstName} ${friends.profile.lastName}`,
@@ -42,53 +69,60 @@ export const CEResponse = ({ ids }) => {
       captionOne: otherSubs.content.sentence,
       myImage: myImage,
       myCaption: mySub.content.sentence,
-      allUsers: users
+      allUsers: users,
+      promptName: experiencePrompt.name,
+      promptQuestion: experiencePrompt.contributionTypes[0].toPass.instruction,
     };
-  
+
     return (
       <div>
-        <Accordion>
+        <Accordion sx={{ border: "5px solid red" }}>
           <AccordionSummary aria-controls="panel1a-content" id="panel1a-header">
-            <Typography>TODO: replace with experience</Typography>
+            <Typography variant="h4">{results.promptName}</Typography>
           </AccordionSummary>
           <AccordionDetails>
-            <Typography>
-              TODO: replace with given prompt<br /> <br />
+            <Typography variant="h5">
+              {results.promptQuestion}
+              <br /> <br />
             </Typography>
             <Card sx={{ display: "flex", flexDirection: "row" }}>
+              {/* <Zoom
+                in={zoom1}
+                style={{ transitionDelay: zoom1 ? "500ms" : "0ms" }}
+                onclick={() => toggle(zoom1, setZoom1)}
+              > */}
               <CardMedia
                 component="img"
-                image={ results.myImage }
+                image={results.myImage}
                 alt="bun bunz"
                 sx={{ width: "50%" }}
               />
+              {/* </Zoom> */}
               <CardContent>
-                <Typography variant="body2" color="text.secondary">
-                  { results.myCaption} - You
+                <Typography variant="body" color="text.secondary">
+                  {results.myCaption} - You
                 </Typography>
               </CardContent>
             </Card>
             <Card sx={{ display: "flex", flexDirection: "row" }}>
               <CardContent>
-                <Typography variant="body2" color="text.secondary">
-                  { results.captionOne } - {results.friendOneName}
+                <Typography variant="body" color="text.secondary">
+                  {results.captionOne} - {results.friendOneName}
                 </Typography>
               </CardContent>
               <CardMedia
                 component="img"
-                image= { results.imageOne}
+                image={results.imageOne}
                 alt="bun bunz"
                 sx={{ width: "50%" }}
               />
             </Card>
           </AccordionDetails>
-        </Accordion> 
+        </Accordion>
       </div>
     );
-    
   } else {
-    console.log("loading submissions and users")
-    return <div></div>
+    console.log("loading submissions and users");
+    return <div></div>;
   }
-  
 };
