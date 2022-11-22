544a953fe3c1ca2ddad9c79e01ccb91608647a31
cindyhu2023 <cindyhu2023@u.northwestern.edu>
2022-11-21 20:46:42 -0600

    update push config (keyData and certData are deprecated)

--- imports/startup/server/push.js
+++ imports/startup/server/push.js
@@ -3,14 +3,14 @@ Push.debug = true;
 Push.Configure({
   apn: {
     production: true,
-    // keyData: Assets.getText("ios/key.pem"), // depreciated for nudelta2015:push
-    // certData: Assets.getText("ios/cert.pem"), // depreciated for nudelta2015:push
-    // pfx: Assets.absoluteFilePath("ios/key.p12"), // depreciated, use P8
-    token: {
-      key: Assets.absoluteFilePath("ios/AuthKey_W9H43BKTXS.p8"),
-      keyId: "W9H43BKTXS",
-      teamId: "823S57WQK3"
-    },
+    key: Assets.getText("ios/key.pem"), // depreciated for nudelta2015:push
+    cert: Assets.getText("ios/cert.pem"), // depreciated for nudelta2015:push
+    pfx: Assets.absoluteFilePath("ios/key.p12"), // depreciated, use P8
+    // token: {
+    //   key: Assets.absoluteFilePath("ios/AuthKey_W9H43BKTXS.p8"),
+    //   keyId: "W9H43BKTXS",
+    //   teamId: "823S57WQK3"
+    // },
     passphrase: "password",
     gateway: "gateway.push.apple.com"
   }
